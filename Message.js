69474b7cc8649837d18bec4e61b19211be2ca4d6
gishoostudio <fakhridardab@gmail.com>
2025-07-21 13:57:26 +0330

    Delete Message.js

--- Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MessageSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  content: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('Message', MessageSchema);
